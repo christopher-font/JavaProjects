52dfb775d64a1a757d309e57c9269161eeb290f9
Christopher Font <64507939+christopher-font@users.noreply.github.com>
2023-10-06 13:58:38 -0400

    Updated random facts API so it can handle facts with quotation marks in it.

--- randomFactAPI.java
+++ randomFactAPI.java
@@ -5,6 +5,7 @@ import java.net.URL;
 public class randomFactAPI {
     public static void main(String[] args) {
         try {
+
             URL url = new URL("https://uselessfacts.jsph.pl/api/v2/facts/random?language=en");
 
             BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
@@ -13,9 +14,17 @@ public class randomFactAPI {
 
             in.close();
 
-            String[] htmlSplit = htmlLine.split("\"");
+            String[] htmlSplit = htmlLine.split(":");
 
-            System.out.println(htmlSplit[7]);
+            String[] htmlLine2 = htmlSplit[2].split("\"");
+
+            htmlLine = "";
+
+            for (int i = 1; i < htmlLine2.length - 2; i++) {
+                htmlLine += htmlLine2[i];
+            }
+
+            System.out.println(htmlLine);
 
         }  catch (Exception e){
             System.out.println(e.getLocalizedMessage());
